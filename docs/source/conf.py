bd8187b15f4a11bef23cfd0c9005b840e544a440
J. Nick Koston <nick@koston.org>
2025-01-18 10:54:13 -1000

    chore: update for Python 3.13

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from typing import Any, List
+from typing import Any
 
 # -- Project information -----------------------------------------------------
 
@@ -40,7 +40,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns: List[Any] = []
+exclude_patterns: list[Any] = []
 
 
 # -- Options for HTML output -------------------------------------------------
